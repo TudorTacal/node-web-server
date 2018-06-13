4aeb3ba05c81d7d3633f94ddc53291c5f1741bfd
Tudor Tacal <t.tacal@elsevier.com>
2018-06-13 22:30:25 +0100

    Craete the web server

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.send({
@@ -23,5 +25,8 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+  
+});
 
