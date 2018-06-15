aa67d930bf810d60a68b410e9366a07a6d779dad
Tudor Tacal <t.tacal@elsevier.com>
2018-06-15 21:02:17 +0100

    Create hbs helpers

--- server.js
+++ server.js
@@ -2,21 +2,29 @@ const express = require('express');
 const hbs = require('hbs');
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs'); 
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'My Web Server',
     welcomeMessage: 'Hello to my website',
-    currentYear: new Date().getFullYear(),
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 })
 
