a9713e9ea161843ca9521ee4225d72da302bfd80
Tudor Tacal <t.tacal@elsevier.com>
2018-06-15 21:19:16 +0100

    Create Express middleware for logging and rendering maintenance page

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs'); 
+
+// app.use((req, res) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  let log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
@@ -13,6 +33,7 @@ hbs.registerHelper('getCurrentYear', () => {
 hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
+
 
 
 app.get('/', (req, res) => {
